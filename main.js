8d3547213db22cc4a9f50b3199e44b0700cffc77
contentkingdom19 <132156518+contentkingdom19@users.noreply.github.com>
2023-05-01 21:59:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let banner_container = document.querySelector(".banner-container");
+console.log(banner_container);
+
+window.addEventListener("load", () => {
+	banner_container.classList.add("banner-container-loaded");
+});
